cad37106fb31f77a2ddf6e53073543b47311e469
Keaten Cavill <Keatencavill@hotmail.com>
2026-04-20 19:24:30 -0600

    Week 10

--- Week10/scripts/trip.js
+++ Week10/scripts/trip.js
@@ -2,7 +2,7 @@
 
 const driver_name = "Keaten";
 
-let distance_miles = 353;
+const distance_miles = 353;
 const mpg = 28;
 const gas_price = 3.90;
 const fuel_capcity = 14;
@@ -37,18 +37,22 @@ const fuel_cost = calculateFuelCost(gallons_needed, gas_price);
 const miles_per_tank = mpg * fuel_capcity;
 
 let miles_traveled = miles_per_tank;
-let stops_needed = 1;
+let stops_number = 1;
 let running_cost = calculateFuelCost(fuel_capcity, gas_price);
 
 // Gas Stop Loop --------------------------------------------
 
 console.log("Gas Stop -----")
 
-while (miles_traveled < miles_per_tank) {
-    console.log(`Stop ${stops_needed}:`);
+while (miles_traveled < total_distance) {
+    console.log(`Stop ${stops_number}:`);
     console.log(`Miles travled so far: ${miles_traveled} miles`);
     console.log(`Running gas cost: $${running_cost.toFixed(2)}`);
     console.log("----------------------");
+
+    stops_number++;
+    miles_traveled += miles_per_tank;
+    running_cost += calculateFuelCost(fuel_capcity, gas_price);
     }
 
 // Final Summary --------------------------------------------
